8c70f17a584525566766beb9eb2ca824b87c072d
Halil Yılmaz <halil.yilmaz@appcent.mobi>
2023-06-05 13:41:22 +0300

    Initial Commit

---
+++ Foody/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Foody
+//
+//  Created by halil yılmaz on 5.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
